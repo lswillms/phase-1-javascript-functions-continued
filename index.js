cc278053ddc68c217a8167f1feae732123b6e740
Latoshia Williams <lswillms@yahoo.com>
2022-07-05 11:45:38 -0500

    commit tree 9f27a454b728923ba6e49e7b1d4b9a72bfd10fc8

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+function saturdayFun(event = "roller-skate") {
+    return (`This Saturday, I want to ${event}!`)
+}
+
+function mondayWork(toDo = "go to the office") {
+    return (`This Monday, I will ${toDo}.`)
+}
+
+function wrapAdjective(result = "*"){
+    return function (emphatic = "a hard worker") {
+        return `You are ${result}${emphatic}${result}!`
+    }
+}
+
